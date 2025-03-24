aa122f38fbe37bf09133084143e5e9f076f445c4
dhiraj <dhirajmymodak22@gmail.com>
2025-03-24 22:02:34 +0530

    final payment updates

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { createOrder } = require('../controllers/payment.controller');
+const { createOrder, verifyPayment } = require('../controllers/payment.controller');
 const paymentRouter = express.Router();
 
 paymentRouter.post('/order',createOrder);
-paymentRouter.post('/verifypayment',)
+paymentRouter.post('/verifypayment',verifyPayment);
 
 module.exports = paymentRouter;
