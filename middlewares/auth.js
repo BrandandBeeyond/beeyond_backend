970345986581e51eca4c4f6232f6cae5206e2b8f
dhiraj <dhirajmymodak22@gmail.com>
2025-04-04 16:19:07 +0530

    adding middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,19 @@
 const jwt = require("jsonwebtoken");
-const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
+const asyncErrorHandler = require("../middlewares/asyncErrorHandler");
 
 const isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
- 
+  const authHeader = req.headers.authorization;
 
-  const token = req.headers.authorization?.split(" ")[1];
+  console.log("Auth Header:", authHeader); // Debugging step
 
-
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res
       .status(401)
       .json({ success: false, message: "Please login to access" });
   }
 
+  const token = authHeader.split(" ")[1];
   try {
-  
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decodedData.id);
     next();
